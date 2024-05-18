f576fd71d684520e0b87ffc648b7de4106136038
Pravee <praveenraman58@gmail.com>
2024-05-18 12:21:00 +0530

    food court

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,8 @@
 const productController = require("express").Router()
 const Product = require("../models/Product")
-const {verifyToken, verifyTokenAdmin} = require('../middlewares/verifyToken')
+const { verifyToken, verifyTokenAdmin } = require('../middlewares/verifyToken')
 
-// get all
-productController.get('/', verifyToken, async(req, res) => {
+productController.get('/', verifyToken, async (req, res) => {
     try {
         const products = await Product.find(req.query)
         return res.status(200).json(products)
@@ -12,24 +11,23 @@ productController.get('/', verifyToken, async(req, res) => {
     }
 })
 
-// get one
-productController.get('/find/:id', verifyToken, async(req, res) => {
-   try {
-    const productId = req.params.id
-    const product = await Product.findById(productId)
-    if(!product){
-        return res.status(500).json({msg: "No product with such id!"})
+
+productController.get('/find/:id', verifyToken, async (req, res) => {
+    try {
+        const productId = req.params.id
+        const product = await Product.findById(productId)
+        if (!product) {
+            return res.status(500).json({ msg: "No product with such id!" })
+        }
+        return res.status(200).json(product)
+    } catch (error) {
+        console.error(error)
     }
-    return res.status(200).json(product)
-   } catch (error) {
-    console.error(error)
-   }
 })
 
-// create product
-productController.post('/', verifyTokenAdmin, async(req, res) => {
+productController.post('/', verifyTokenAdmin, async (req, res) => {
     try {
-        const newProduct = await Product.create({...req.body})
+        const newProduct = await Product.create({ ...req.body })
         return res.status(201).json(newProduct)
     } catch (error) {
         console.error(error)
